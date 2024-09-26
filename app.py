558051fc55d4618e9d691b2d5fdafd485eb01cb1
Brossal <75007211+Brossal@users.noreply.github.com>
2024-09-25 23:54:34 -0300

    ss

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-
+import os
 
 def salida(text:str):
     lista = text.split(" ")
@@ -10,6 +10,6 @@ def salida(text:str):
 respuesta = st.text_input("Escribe cualquier cosa.")
 if st.button("Enviar"):
     salida(respuesta)
-
-
+if st.button("listdir"):
+    st.write(os.listdir())
 data = st.file_uploader("cargar")
